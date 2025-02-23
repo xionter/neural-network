615e4b7cb82d82b43df812592fcdcc1d81004b26
xionter <andrey0502255@gmail.com>
2025-02-23 18:58:00 +0500

    TODO: do something with data field, add learning, abstract model(not only xor), ability to make submatrix, back propogation

--- src/Framework.cs
+++ src/Framework.cs
@@ -21,27 +21,21 @@ public unsafe class Mat
         set => Elements[i * Cols + j] = value;
     }       //взрыв мозга, лазеры из глаз
     
-    
-
     public Mat GetRow(int i)
     {
         var row = new float[Cols];
         Array.Copy(Elements, i * Cols, row, 0, Cols);
         return new Mat(1, Cols, row);
     }
+
     public static void Add(Mat a, Mat b)
     {
         if(a.Rows != b.Rows) throw new InvalidOperationException("Matrix rows dont match");
         if(a.Cols != b.Cols) throw new InvalidOperationException("Matrix cols dont match");
     
         for(int y = 0; y < a.Rows; ++y)
-        {
             for(int x = 0; x < a.Cols; ++x)
-            {
                 a[y,x]+= b[y,x];
-            }
-        }
-
     }
     public static void Mult(Mat dst, Mat a, Mat b)
     {
@@ -50,15 +44,14 @@ public unsafe class Mat
         if(dst.Cols != b.Cols) throw new InvalidOperationException("Matrix sizes dont match");
 
         for(int y = 0; y < a.Rows; ++y)
-        {
             for(int x = 0; x < b.Cols;++x)
-            {
                 for(int k = 0; k < a.Cols; ++k)
-                {
+                    dst[y,x] = 0;
+
+        for(int y = 0; y < a.Rows; ++y)
+            for(int x = 0; x < b.Cols;++x)
+                for(int k = 0; k < a.Cols; ++k)
                     dst[y,x] += a[y,k] * b[k, x];
-                }
-            }
-        }
     }
     public void Print()
     {
@@ -73,36 +66,26 @@ public unsafe class Mat
 
     }
 
-    public void Rand(int low, int high)
+    public void Rand(float low, float high)
     {
         for(int y = 0; y < Rows; ++y)
-        {
             for(int x = 0; x < Cols; ++x)
-            {
-                this[y,x] = Func.RandF() * (low - high) + low;
-            }
-        }
+                this[y,x] = Func.RandF() * (high - low) + low;
     }
 
     public static void Sigmoid(Mat m)
     {
         for(int y = 0; y < m.Rows; ++y)
-        {
             for(int x = 0; x < m.Cols; ++x)
-            {
                 m[y,x] = Func.Sigmoidf(m[y,x]);
-            }
-        }
     }
-
-
 }
 public class Func
 {
     public static float RandF()
     {
         var rnd = new Random();
-        return rnd.Next(0, int.MaxValue) * 1.0f / int.MaxValue;
+        return rnd.NextSingle();
     }
     public static float Sigmoidf(float x) =>  1.0f / (MathF.Exp(-x) + 1.0f);
 }
--- src/Program.cs
+++ src/Program.cs
@@ -2,13 +2,13 @@
 
 class Program
 {    
-    public float[] td = {
+    public static float[] td = {
         0, 0, 0, 
-        1, 0, 0,
-        0, 1, 0,
+        1, 0, 1,
+        0, 1, 1,
         1, 1, 0,
     }; 
-    public struct Xor
+    public class Xor
     {
         public Mat a0;
         public Mat w1,b1,a1;
@@ -21,38 +21,38 @@ class Program
             w1 = new Mat(2,2);
             b1 = new Mat(1,2);
             a1 = new Mat(1,2);
-            w1.Rand(0, 1); b1.Rand(0, 1);
-
+            a2 = new Mat(1,1);
             w2 = new Mat(2,1);
             b2 = new Mat(1,1);
-            a2 = new Mat(1,1);
-            w1.Rand(0, 1); b1.Rand(0, 1);
+
+            w2.Rand(-0.5f, 0.5f); b2.Rand(-0.5f, 0.5f);
+            w1.Rand(-0.5f, 0.5f); b1.Rand(-0.5f, 0.5f);
         }
     }
 
     public static float cost(Xor model, Mat ti, Mat to)
     {
-        float cost = 0.0f;
+        float c = 0.0f;
         int n = to.Rows;
         for(int i = 0; i < n; ++i)
         {
             var x = ti.GetRow(i);
             var y = to.GetRow(i);
+            var save = model.a0;
             model.a0 = x;
-            forward_xor(model);
-
+            ForwardXor(model);
             for(int j = 0; j < model.a2.Cols; ++j)
             {
-                float d = model.a2[i,j] - y[i,j];
-                cost += d*d;
+                float d = model.a2[0,j] - y[0,j];
+                c += d*d;
             }
+            model.a0 = save;
         }
-        return cost;
+        return c;
     }
 
-    static void forward_xor(Xor model)
+    public static void ForwardXor(Xor model)
     {
-
         Mat.Mult(model.a1, model.a0, model.w1);
         Mat.Add(model.a1, model.b1);
         Mat.Sigmoid(model.a1);
@@ -60,11 +60,112 @@ class Program
         Mat.Mult(model.a2, model.a1, model.w2);
         Mat.Add(model.a2, model.b2);
         Mat.Sigmoid(model.a2);
+    }
+    public static void FiniteDiff(Xor m, Xor g, float eps, Mat ti, Mat to)
+    {
+        float temp;
+
+        float c = cost(m, ti, to);
+        for(var i = 0; i < m.w1.Rows; ++i)
+        {
+            for(var j = 0; j < m.w1.Cols; ++j)
+            {
+                temp = m.w1[i,j];
+                m.w1[i,j] +=eps;
+                g.w1[i,j] = (cost(m,ti,to) - c) / eps;
+                m.w1[i,j] = temp;
+            }
+        }
+
+        for(var i = 0; i < m.b1.Rows; ++i)
+        {
+            for(var j = 0; j < m.b1.Cols; ++j)
+            {
+                temp = m.b1[i,j];
+                m.b1[i,j] +=eps;
+                g.b1[i,j] = (cost(m,ti,to) - c) / eps;
+                m.b1[i,j] = temp;
+            }
+        }
+        for(var i = 0; i < m.w2.Rows; ++i)
+        {
+            for(var j = 0; j < m.w2.Cols; ++j)
+            {
+                temp = m.w2[i,j];
+                m.w2[i,j] +=eps;
+                g.w2[i,j] = (cost(m,ti,to) - c) / eps;
+                m.w2[i,j] = temp;
+            }
+        }
+
+        for(var i = 0; i < m.b2.Rows; ++i)
+        {
+            for(var j = 0; j < m.b2.Cols; ++j)
+            {
+                temp = m.b2[i,j];
+                m.b2[i,j] +=eps;
+                g.b2[i,j] = (cost(m,ti,to) - c) / eps;
+                m.b2[i,j] = temp;
+            }
+        }
+    }
+
+    public static void Learn(Xor m, Xor g, float rate)
+    {
+        for(var i = 0; i < m.w1.Rows; ++i)
+            for(var j = 0; j < m.w1.Cols; ++j)
+                m.w1[i,j] -= g.w1[i,j] * rate;
+
+        for(var i = 0; i < m.b1.Rows; ++i)
+            for(var j = 0; j < m.b1.Cols; ++j)
+                m.b1[i,j] -= g.b1[i,j] * rate;
+
+        for(var i = 0; i < m.w2.Rows; ++i)
+            for(var j = 0; j < m.w2.Cols; ++j)
+                m.w2[i,j] -= g.w2[i,j] * rate;
+
+        for(var i = 0; i < m.b2.Rows; ++i)
+            for(var j = 0; j < m.b2.Cols; ++j)
+                m.b2[i,j] -= g.b2[i,j] * rate;
 
     }
     public static void Main()
     {
-        var m = new Xor();
+        var numCols = 3; 
+        var numRows = td.Length / numCols;
 
+        var inp = new float[numRows * 2];
+        var outp = new float[numRows];
+
+        for(int i = 0; i < numRows; ++i)
+        {
+            inp[i*2] = td[i*3];
+            inp[i*2 + 1] = td[i*3 + 1];
+            outp[i] = td[i*3 + 2];
+        }
+        var ti = new Mat(numRows, 2, inp);
+        var to = new Mat(numRows, 1, outp);
+        
+        var m = new Xor();
+        var g = new Xor();
+        
+        float eps = 1e-1f;
+        float rate = 1e-1f; 
+        for(int i = 0; i < 10000; ++i)
+        {
+            FiniteDiff(m, g, eps, ti, to);
+            Learn(m, g, rate);
+        }
+
+        for(int i = 0; i < 2; ++i)
+        {
+            for(int j = 0; j < 2; ++j)
+            {
+                m.a0[0,0] = i;
+                m.a0[0,1] = j;
+                ForwardXor(m);
+                Console.WriteLine($"{i} ^ {j} = {m.a2[0,0]}");
+            }
+        }
     }
 }
